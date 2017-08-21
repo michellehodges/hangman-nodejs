fabd266faec2e13c2a0d8a1800a5f343e7d25478
michellehodges <mischamangan@gmail.com>
2017-08-21 13:46:30 -0400

    Underscores appropriate to number of letters in words is now rendered in DOM.

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const users = [
   { username: "maria", score: 20, tries: 8 }
 ]
 
-const randomWordSplit = [];
-const randomWordUnderscores = [];
+let randomWordSplit = null;
+let randomWordUnderscores = null;
 let words = null;
 let hiddenWord = null;
 
@@ -43,6 +43,7 @@ server.get('/play', function(request, response) {
       score: request.session.who.score,
       tries: request.session.who.tries,
       guessedLetters: request.session.who.guesses,
+      underscores: request.session.who.underscores
       // level: request.session.who.level,
     });
   } else {
@@ -68,45 +69,35 @@ server.post('/start', function(request, response) {
       tries: 8,
       guessedLetters: [''],
     };
+
     users.push(user);
     request.session.who = user;
+
+    words = filesystem.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n")
+    request.session.who.randomWord = words[Math.floor(Math.random()*words.length)];
+    randomWordSplit = request.session.who.randomWord.split('');
+    randomWordUnderscores = Array(randomWordSplit.length+1).join('_').split('');
+    request.session.who.underscores = randomWordUnderscores;
+
     response.redirect('/play')
-  } else {
-    response.redirect('/')
-  }
-  //choose a random word
-  words = filesystem.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n")
-  request.session.who.randomWord = words[Math.floor(Math.random()*words.length)];
-
-  //split the random word into an array of characters
-  randomWordSplit = request.session.who.randomWord.split('');
-
-  //print out underscores with randomword.split.length
-  randomWordUnderscores = Array(randomWordSplit.length).join('_ ')
-
-  console.log(randomWordSplit);
-  console.log(randomWordUnderscores);
-  console.log(request.session.who.randomWord);
-  console.log(users)
-
+    } else {
+      response.redirect('/')
+      }
 })
 
 server.post('/guess', function(request, response) {
   if ((request.session.who.tries > 1) && (request.body.guess !== '')) {
-    //1. Decrease the number of tries
     request.session.who.tries -= 1;
-    //3. Check If request.body.guess exists under randomWordSplit,
+    request.session.who.guessedLetters.push(request.body.guess);
+
     if (randomWordSplit.indexOf(request.body.guess) !== -1) {
-      guessedLetters.push(request.body.guess);
+
       //replace randomWordUnderscores (with matching index number as randomWordSplit[i]) with request.body.guess
       //add 10 to scores
-    } else {
-      response.redirect('/play')
     }
 
     response.redirect('/play');
-    console.log('Guessed letter array: ' + guessedLetters)
-    console.log (request.session.who.tries);
+    console.log('Guessed letter array: ' + request.session.who.guessedLetters)
 
   } else if (request.body.guess === '') {
     response.redirect('/play');
